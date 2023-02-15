7fd281c5f74358ae5fd878ff467cdb457a9a8b9e
xtombrock <xtombrock1989@gmail.com>
2023-02-15 18:23:37 +0000

    buttons

--- client/src/components/header/CTA.jsx
+++ client/src/components/header/CTA.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
 
 function CTA() {
   return (
     <div className='grid grid-flow-col space-x-0 w-full gap-4'>
-      <Link className='bg-eco-green dark:bg-white border-2 rounded-md px-4 py-2 text-center text-white dark:text-eco-green dark:border-eco-green hover:text-eco-green-dark hover:bg-white hover:border-eco-green'>
+      <Link className='bg-eco-green dark:bg-eco-green-dark border-2 rounded-md px-4 py-2 text-center text-white dark:text-eco-green dark:border-eco-green hover:text-eco-green-dark active:text-slate-100 hover:bg-white hover:border-eco-green'>
         See More
       </Link>
       <Link className='border-2 rounded-md text-center text-eco-green-dark border-eco-green hover:bg-eco-green px-4 py-2 hover:text-white dark:text-eco-green hover:border-eco-green-dark'>
