668719768ba9b5485e8c3cb2847d7f9b0e89a9b8
xtombrock <xtombrock1989@gmail.com>
2023-02-17 18:11:21 +0000

    dark

--- client/src/components/about/About.jsx
+++ client/src/components/about/About.jsx
@@ -11,7 +11,7 @@ function About() {
     <>
       <section
         id='about'
-        className='px-4 py-8 sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8'
+        className='dark:bg-black px-4 py-8 sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8'
       >
         <div>
           <h2 className='text-eco-green'>About</h2>
