b723ab843306511770a5c91d3fd1f57c5bfcaa74
xtombrock <xtombrock1989@gmail.com>
2023-02-17 18:10:42 +0000

    save

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,8 @@ import { Route, Routes } from 'react-router-dom';
 import Home from './pages/home/Home'
 import Login from './users/Login';
 import Register from './users/Register';
+// Prestyled components
+import './styles/components.css'
 
 function App() {
   return (
