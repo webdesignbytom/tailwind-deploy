7fd5c1fd8d1dd7d9fae7a84ad8e2772e1c1c3ebe
xtombrock <xtombrock1989@gmail.com>
2023-02-22 00:55:15 +0000

    funding

--- client/src/components/goals/Goal.jsx
+++ client/src/components/goals/Goal.jsx
@@ -9,7 +9,7 @@ function Goal({ goal }) {
             {goal.title}
           </h3>
         </div>
-        <p className='secondary__container'>{goal.content}</p>
+        <p className='whiteText__container'>{goal.content}</p>
       </div>
       <div className='border-2 group-hover:border-2 border-white group-hover:border-eco-green-dark rounded-md overflow-hidden m-2 box-content md:object-cover lg:object-cover'>
         <img className='w-full h-[250px]' src={goal.image} alt={goal.title} />
--- client/src/utils/FundingOptions.jsx
+++ client/src/utils/FundingOptions.jsx
@@ -2,36 +2,45 @@
 import Games from '../assets/img/games.jpg'
 import FundRaising from '../assets/img/fundraising.png'
 import SocialMedia from '../assets/img/socialMedia.jpg'
+import Power from '../assets/img/power.jpg'
+import Lottery from '../assets/img/lottery.jpg'
+import MyeaLogo from '../assets/img/myea-logo.svg'
 
 export const FundingOptions = [
     {
         title: 'Internet Fundraising',
         image: FundRaising,
-        content: 'lorem sff dfnfo asfao af fadf adfijfa asfiiafaoad fasifjaf fiasfas fsifjasfnaf fiasf fidfif fifofo fdjndovnodnvs vj sdvdjvnoidsvo vd svjsdvnsdkv jdsv sdkl dk dokvsd vjdjvivjod vjsdv skdv sjdov osd vj vdjd vjd jsd vksdjv kj v dv'
+        content: "You have value! And you can help us without looking up from your phone. All you have to do is use, look at, view or share our content from websites and social media. That way we can earn from advertising and data to fund our causes. We don't want to ask anyone for money. Just provide a service that will help or entertain you which we can earn off.",
+        link: 'http://www.'
     },
     {
         title: 'Social Media and Video Content',
         image: SocialMedia,
-        content: 'lorem sff dfnfo asfao af fadf adfijfa asfiiafaoad fasifjaf fiasfas fsifjasfnaf fiasf fidfif fifofo fdjndovnodnvs vj sdvdjvnoidsvo vd svjsdvnsdkv jdsv sdkl dk dokvsd vjdjvivjod vjsdv skdv sjdov osd vj vdjd vjd jsd vksdjv kj v dv'
+        content: "With your help visiting our social media accounts and follow us. We can increase our online values and earn through advertising. We have done what we can to make this easy for you. A page that displays all our latest medias in one place. Where you can view and like our content. Every view increase the pot and as we grow we can charge more and more for our advertising space.",
+        link: 'http://www.'
     },
     {
         title: 'Games and Gaming',
         image: Games,
-        content: 'lorem sff dfnfo asfao af fadf adfijfa asfiiafaoad fasifjaf fiasfas fsifjasfnaf fiasf fidfif fifofo fdjndovnodnvs vj sdvdjvnoidsvo vd svjsdvnsdkv jdsv sdkl dk dokvsd vjdjvivjod vjsdv skdv sjdov osd vj vdjd vjd jsd vksdjv kj v dv'
+        content: "As part of the entertainment package, we have made some games for you all to enjoy. Based on classic popular games you can play while bored, and modern popular games such as idle clickers and town builders. This content earns us through adverts placed occasionally thoughtout the games or if players want to pay to play.",
+        link: 'http://www.'
     },
-    // {
-    //     title: 'Funding 4',
-    //     image: 'https://',
-    //     content: 'lorem sff dfnfo asfao af fadf adfijfa asfiiafaoad fasifjaf fiasfas fsifjasfnaf fiasf fidfif fifofo fdjndovnodnvs vj sdvdjvnoidsvo vd svjsdvnsdkv jdsv sdkl dk dokvsd vjdjvivjod vjsdv skdv sjdov osd vj vdjd vjd jsd vksdjv kj v dv'
-    // },
-    // {
-    //     title: 'Funding 5',
-    //     image: 'https://',
-    //     content: 'lorem sff dfnfo asfao af fadf adfijfa asfiiafaoad fasifjaf fiasfas fsifjasfnaf fiasf fidfif fifofo fdjndovnodnvs vj sdvdjvnoidsvo vd svjsdvnsdkv jdsv sdkl dk dokvsd vjdjvivjod vjsdv skdv sjdov osd vj vdjd vjd jsd vksdjv kj v dv'
-    // },
-    // {
-    //     title: 'Funding 6',
-    //     image: 'https://',
-    //     content: 'lorem sff dfnfo asfao af fadf adfijfa asfiiafaoad fasifjaf fiasfas fsifjasfnaf fiasf fidfif fifofo fdjndovnodnvs vj sdvdjvnoidsvo vd svjsdvnsdkv jdsv sdkl dk dokvsd vjdjvivjod vjsdv skdv sjdov osd vj vdjd vjd jsd vksdjv kj v dv'
-    // }
+    {
+        title: 'myecohome',
+        image: Power,
+        content: "Myecohome is a web service we provide. To earn us money everytime you open your browser without you being annoyed. Our pages are a collection of fun games, apps or notice boards that you set as your homepage on all your devices. Each page has a small advert attached and we earn from the page being open. And the environment thanks you. To make it more fun some home pages are integrated with ways to share your data on social media.",
+        link: 'http://www.'
+    },
+    {
+        title: 'myecoshop',
+        image: MyeaLogo,
+        content: "Visit the myecoshop website! We have a simple shop which is practically free to run. Being online only and using our in house web devs it costs almost nothing to start and host with no staff needed to manage it. You can support us by buying products and knowing all the profit from each product goes towards helping people help the planet. Each product makes us over £1 and we never have to stock any products, by just acting as a sales platform.",
+        link: 'http://www.myecoapp.shop'
+
+    },
+    {
+        title: 'Lottery',
+        image: Lottery,
+        content: "By hosting an online lottery we can hope to raise huge amounts of money. The lottery will guarantee that if no one wins, everyone who bought a ticket gets one chance to win the pot in a second draw. The prize will be a percentage of the total raised from selling tickets. We aim to sell 1 million tickets at 20p each a week. Raising £200,000 from sales. Meaning each week we can afford to build a small park or install 40 house worth of solar panels!"
+    }
 ]
