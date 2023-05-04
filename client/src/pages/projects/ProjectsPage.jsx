b99b1305b6b3396e84862a32c1c975a4973da56f
Thomas Brockington <xtombrock1989@gmail.com>
2023-05-04 11:59:47 +0100

    projects

--- client/src/pages/projects/ProjectsPage.jsx
+++ client/src/pages/projects/ProjectsPage.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from 'react';
+// Components
+import Navbar from '../../components/navigation/Navbar';
 
 function ProjectsPage() {
   return (
-    <div>ProjectsPage</div>
-  )
+    <div className='bg-white dark:bg-black min-h-screen lg:max-h-screen lg:overflow-hidden lg:grid-rows-reg grid'>
+      <Navbar />
+      <main className='grid'></main>
+    </div>
+  );
 }
 
-export default ProjectsPage
+export default ProjectsPage;
