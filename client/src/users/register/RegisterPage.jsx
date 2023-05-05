784c2504d14d8f1553506b3800c96ac765cc44d9
Thomas Brockington <xtombrock1989@gmail.com>
2023-05-06 00:18:42 +0100

    save

--- client/src/users/register/RegisterPage.jsx
+++ client/src/users/register/RegisterPage.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 // Components
 import Navbar from '../../components/navigation/Navbar';
 
@@ -6,10 +6,9 @@ function RegisterPage() {
   return (
     <div className='bg-white dark:bg-black min-h-screen lg:max-h-screen lg:overflow-hidden lg:grid-rows-reg grid'>
       <Navbar />
-      <main className='grid'>
-      </main>
+      <main className='grid'></main>
     </div>
-  )
+  );
 }
 
-export default RegisterPage
+export default RegisterPage;
