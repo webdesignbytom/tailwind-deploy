84f2a56f4242ab89e04633cffed750d8998f5425
Thomas Brockington <xtombrock1989@gmail.com>
2023-05-06 00:26:07 +0100

    buttons

--- client/src/components/home/HeaderCTA.jsx
+++ client/src/components/home/HeaderCTA.jsx
@@ -6,7 +6,7 @@ function HeaderCTA() {
     <div className='grid grid-flow-col gap-6 h-fit'>
       <Link
         to='/our-goals'
-        className='p-2 shadow-[4.0px_8.0px_8.0px_rgba(0,0,0,0.38)] outline-2 no__highlights outline-black outline rounded text-center flex items-center justify-center bg-main-colour hover:bg-colour-pale active:scale-110 duration-200 ease-in-out lg:text-base 2xl:text-xl font-semibold text-gray-100'
+        className='p-2 shadow-[4.0px_8.0px_8.0px_rgba(0,0,0,0.38)] outline-2 no__highlights outline-colour-dark outline rounded text-center flex items-center justify-center bg-main-colour hover:bg-colour-dark hover:text-gray-100 active:scale-110 duration-200 ease-in-out lg:text-base 2xl:text-xl font-semibold text-gray-100'
       >
         <article>
           <h3>GOALS</h3>
@@ -14,7 +14,7 @@ function HeaderCTA() {
       </Link>
       <Link
         to='/funding'
-        className='p-2 shadow-[4.0px_8.0px_8.0px_rgba(0,0,0,0.38)] outline-2 no__highlights outline-black outline rounded text-center flex items-center justify-center bg-gray-100 hover:bg-colour-pale active:scale-110 duration-200 ease-in-out lg:text-base 2xl:text-xl font-semibold text-gray-800'
+        className='p-2 shadow-[4.0px_8.0px_8.0px_rgba(0,0,0,0.38)] outline-2 no__highlights outline-main-colour outline rounded text-center flex items-center justify-center bg-gray-100 hover:bg-colour-dark hover:text-gray-100 active:scale-110 duration-200 ease-in-out lg:text-base 2xl:text-xl font-semibold text-main-colour'
       >
         <article>
           <h3>FUNDING</h3>
@@ -22,7 +22,7 @@ function HeaderCTA() {
       </Link>
       <Link
         to='/about-us'
-        className='p-2 shadow-[4.0px_8.0px_8.0px_rgba(0,0,0,0.38)] outline-2 no__highlights outline-black outline rounded text-center flex items-center justify-center bg-gray-100 hover:bg-colour-pale active:scale-110 duration-200 ease-in-out lg:text-base 2xl:text-xl font-semibold text-gray-800'
+        className='p-2 shadow-[4.0px_8.0px_8.0px_rgba(0,0,0,0.38)] outline-2 no__highlights outline-main-colour outline rounded text-center flex items-center justify-center bg-gray-100 hover:bg-colour-dark hover:text-gray-100 active:scale-110 duration-200 ease-in-out lg:text-base 2xl:text-xl font-semibold text-main-colour'
       >
         <article>
           <h3>ABOUT</h3>
