aa31d7535e22c997489d8a7586e70b7402725165
Thomas Brockington <xtombrock1989@gmail.com>
2024-01-21 03:14:29 +0000

    save

--- client/src/components/games/bioClicker/containers/BioClickerPointsDataContainer.jsx
+++ client/src/components/games/bioClicker/containers/BioClickerPointsDataContainer.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from 'react';
 
 function BioClickerPointsDataContainer() {
   return (
-    <section className='grid h-[60px] bg-[#00000060] w-full absolute top-0'>BioClickerPointsDataContainer</section>
-  )
+    <section className='grid h-[60px] bg-[#00000060] w-full absolute top-0'>
+      <div className='grid grid-cols-3 gap-2 items-center px-2'>
+        <div className='grid outline outline-1 outline-black rouned-xl py-1 px-1 items-center justify-center'>PPS</div>
+        <div className='grid outline outline-1 outline-black rouned-xl py-1 px-1 items-center justify-center'>SCORE</div>
+        <div className='grid outline outline-1 outline-black rouned-xl py-1 px-1 items-center justify-center'>PPC</div>
+      </div>
+    </section>
+  );
 }
 
-export default BioClickerPointsDataContainer
+export default BioClickerPointsDataContainer;
