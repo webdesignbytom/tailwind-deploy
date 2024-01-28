4eed5f058f5e624574cb63743d6b8eeee6e2c17c
Thomas Brockington <xtombrock1989@gmail.com>
2024-01-28 21:20:10 +0000

    save

--- client/src/components/timeline/TimelineCard.jsx
+++ client/src/components/timeline/TimelineCard.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
 
-function TimelineCard({ isOdd, item }) {
+function TimelineCard({ isOdd, item, key }) {
   if (isOdd) {
     return (
       <article className='grid w-full h-fit '>
-        <div className='grid w-1/2 px-2 py-2 mx-auto border-b-2 border-l-2 h-full border-black border-solid'>
+        <div className='grid grid-cols-reg gap-x-1 w-1/2 px-2 py-2 mx-auto border-b-2 border-l-2 border-t-2 rounded-xl h-full border-black border-solid'>
+          <div
+            className={`outline outline-1 outline-black  rounded-lg p-1 w-full min-w-[90px]`}
+          >
+            <div
+              className={`grid items-center justify-center h-full bg-blue-300
+              ${item.id === 3 && '!bg-purple-300'} 
+              ${item.id === 5 && '!bg-green-300'}  rounded-lg`}
+            >
+              <span className='font-bold'>£{item.cost}</span>
+            </div>
+          </div>
           <div className='outline outline-1 outline-black rounded-lg p-1 w-1/2'>
             <div className='grid bg-yellow-200 w-full rounded-lg px-4 py-2 '>
               <div className='font-semibold'>{item.title}</div>
@@ -18,7 +29,7 @@ function TimelineCard({ isOdd, item }) {
   if (!isOdd) {
     return (
       <article className='grid w-full h-fit'>
-        <div className='grid w-1/2 px-2 py-2 mx-auto justify-items-end border-b-2 border-r-2 h-full border-black border-solid '>
+        <div className='grid w-1/2 px-2 py-2 mx-auto justify-items-end  rounded-xl h-full border-black border-solid '>
           <div className='outline outline-1 outline-black rounded-lg p-1 w-1/2'>
             <div className='grid bg-yellow-200 w-full rounded-lg px-4 py-2 '>
               <div className='font-semibold'>{item.title}</div>
